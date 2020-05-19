a3062e8f41b0701cc634701a6c0c85b3d1973b85
goodchildinit <okerekechigoziev@gmail.com>
2020-05-19 23:52:27 +0200

    Multi auth guard for admin

--- routes/web.php
+++ routes/web.php
@@ -22,5 +22,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::prefix('/admin')->namespace('Admin')->group(function() {
   // All the admin routes will be defined here :-
   Route::match(['get','post'],'/', 'AdminController@login');
-  Route::get('dashboard', 'AdminController@dashboard');
+  Route::group(['middleware' => ['admin']], function () {
+    Route::get('dashboard', 'AdminController@dashboard');
+  });
 });
